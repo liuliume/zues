4a2e4a95635520d4e5a5c485517d021c0559a539
laozhiyi <lzy.clement@gmail.com>
2015-09-13 21:40:09 +0800

    modify

--- src/main/webapp/resources/scripts/pages/course/courseIndex.js
+++ src/main/webapp/resources/scripts/pages/course/courseIndex.js
@@ -3,6 +3,29 @@ var Course = function() {
 	var createOrUpdate = function() {
 		urls = "/course/createOrUpdate";
 
+
+        if ($("#course_name").val() == null || $("#course_name").val() == "") {
+            $("#course_name_error").css("display", "block");
+            $("#course_name").css("border-color", "red");
+            return;
+        }
+        if ($("#course_describe").val() == null || $("#course_describe").val() == "") {
+            $("#errorCourseDescribe").css("display", "block");
+            $("#course_describe").css("border-color", "red");
+            return;
+        }
+        if ($("#expense").val() == null || $("#expense").val() == "") {
+            $("#expense_error").css("display", "block");
+            $("#expense").css("border-color", "red");
+            return;
+        }
+        if(isNaN($("#expense").val())){
+            $("#expense_error").text("价格系数必须为数字");
+            $("#expense_error").css("display","block");
+            $("#expense").css("border-color","red");
+            return false;
+        }
+
 		$.ajax({
 			url:urls,
 			type:"POST",
